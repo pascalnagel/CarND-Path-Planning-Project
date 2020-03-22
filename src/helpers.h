d0af114ce180a665f8a91ff5ac9a245c7bced6b3
Pascal Nagel <me@pnagel.net>
2020-03-22 12:23:36 +0100

    code done, validated on 20 miles w/o incident

--- src/helpers.h
+++ src/helpers.h
@@ -4,10 +4,25 @@
 #include <math.h>
 #include <string>
 #include <vector>
+#include "Eigen-3.3/Eigen/Core"
+#include "Eigen-3.3/Eigen/QR"
+#include "spline.h"
+#include <algorithm>
 
 // for convenience
 using std::string;
 using std::vector;
+using Eigen::Matrix3f;
+using Eigen::Vector3f;
+
+// The max s value before wrapping around the track back to 0
+double max_s = 6945.554;
+
+vector<double> JMT(vector<double> &start, vector<double> &end, double T);
+vector<double> getXY(double s, double d, const vector<double> &maps_s, 
+                     const vector<double> &maps_x, const vector<double> &maps_y,
+                     tk::spline x_spl, tk::spline y_spl);
+
 
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
@@ -24,15 +39,259 @@ string hasData(string s) {
   return "";
 }
 
-//
-// Helper functions related to waypoints and converting from XY to Frenet
-//   or vice versa
-//
+
+struct vehicle {
+  int id;
+  double s;
+  double d;
+  double v;
+};
+
 
 // For converting back and forth between radians and degrees.
 constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
+
+
+// Determine possible lane changes
+vector<string> get_possible_next_states(int current_lane){
+  vector<string> possible_next_states;
+  possible_next_states.push_back("KL");
+  if (current_lane == 0){
+    possible_next_states.push_back("LCR");
+    possible_next_states.push_back("DLCR");
+  } else if (current_lane == 2){
+    possible_next_states.push_back("LCL");
+    possible_next_states.push_back("DLCL");
+  } else {
+    possible_next_states.push_back("LCR");
+    possible_next_states.push_back("LCL");
+  }
+  return possible_next_states;
+}
+
+
+// Rewards larger average velocities during a maneuver
+double efficiency_cost(vector<vector<double>> trajectory, double T){
+  double begin_s = trajectory[2][0];
+  double end_s = trajectory[2][trajectory[2].size()-1];
+  double cost = 1.0 - 1.0 / (1.0 + exp(-(end_s - begin_s)/T/10.0));
+  //std::cout << "Efficiency cost: " << cost << std::endl;
+  return cost;
+}
+
+
+// Penalizes planning to drive with too little safety distance
+double inlane_safety_cost(vector<vector<double>> trajectory, vector<vehicle> vehicles, double T){
+  double start_s = trajectory[2][0];
+  double end_s = trajectory[2][trajectory[2].size()-1];
+  double start_d = trajectory[3][0];
+  double end_d = trajectory[3][trajectory[3].size()-1];
+  vehicle closest_vehicle_tar_lane;
+  double dist_tar_lane = 9999;
+  for (int i=0; i<vehicles.size(); ++i){
+    // Check if a vehicle is in our target lane
+    if ((fabs(vehicles[i].d - end_d) < 1.0)){
+      // Check if the vehicle will be in front of us in time T
+      if (vehicles[i].s + vehicles[i].v*T > end_s){
+        double dist = vehicles[i].s + vehicles[i].v*T - end_s;
+        if (dist < dist_tar_lane){
+          closest_vehicle_tar_lane = vehicles[i];
+          dist_tar_lane = dist;
+          //std::cout << "Distance to vehicle in front: " << dist_tar_lane << std::endl;
+        }
+      }
+    }
+  }
+  double cost = 1.0 - 1.0 / (1.0 + exp(-(dist_tar_lane/10.0)));
+  //std::cout << "Inlane safety_cost: " << cost << std::endl;
+  return cost;
+}
+
+
+// Penalizes changing lanes, when the target lane is occupied
+double lanechange_safety_cost(vector<vector<double>> trajectory, vector<vehicle> vehicles, double T){
+  double start_s = trajectory[2][0];
+  double end_s = trajectory[2][trajectory[2].size()-1];
+  double start_d = trajectory[3][0];
+  double end_d = trajectory[3][trajectory[3].size()-1];
+  double cost = 0;
+
+  if (fabs(end_d - start_d) > 1.0){ // if lane is changed in the trajectory
+    for (int i=0; i<vehicles.size(); ++i){
+      //check for vehicles on the target lane
+      if (fabs(vehicles[i].d - end_d) < 1.0){
+        // Check if a vehicle will be within [-25m, +12m] of our target s-position on the target lane 
+        // at the end of the maneuver
+        if ((vehicles[i].s + vehicles[i].v*T > end_s - 25.0) && (vehicles[i].s + vehicles[i].v*T < end_s + 12.0)){
+          cost = 1;
+        }
+      }
+    }
+  }
+  //std::cout << "Lane change safety_cost: " << cost << std::endl;
+  return cost;
+}
+
+
+// Penalizes double lane changes, when the middle lane is occupied
+double doublelanechange_safety_cost(vector<vector<double>> trajectory, vector<vehicle> vehicles, double T){
+  double start_s = trajectory[2][0];
+  double end_s = trajectory[2][trajectory[2].size()-1];
+  double start_d = trajectory[3][0];
+  double end_d = trajectory[3][trajectory[3].size()-1];
+  double cost = 0;
+
+  if (fabs(end_d - start_d) > 6.0){ // if double lane change is executed in the trajectory
+    for (int i=0; i<vehicles.size(); ++i){
+      //check for vehicles on the middle lane
+      if (fabs(vehicles[i].d - 6.0) < 1.0){
+        // Check if a vehicle will be within [-50m, +50m] of our s-position when crossing the middle lane
+        double crossing_s = (end_s + start_s)/2.0;
+        if ((vehicles[i].s + vehicles[i].v*T > crossing_s - 50.0) && (vehicles[i].s + vehicles[i].v*T < crossing_s + 50.0)){
+          cost = 1;
+        }
+      }
+    }
+  }
+  //std::cout << "Double lane change safety_cost: " << cost << std::endl;
+  return cost;
+}
+
+
+// Penalizes unnecessary lane changes
+double lanechange_cost(vector<vector<double>> trajectory){
+  double start_d = trajectory[3][0];
+  double end_d = trajectory[3][trajectory[3].size()-1];
+  double cost;
+  // if lane is changed in the trajectory
+  if (fabs(end_d - start_d) > 1.0){
+    cost = 1;
+  } else {
+    cost = 0;
+  }
+  //std::cout << "Lane change cost: " << cost << std::endl;
+  return cost;
+}
+
+
+double eval_cost(vector<vector<double>> trajectory, vector<vehicle> vehicles, vector<double> weights, double T){
+  return weights[0]*efficiency_cost(trajectory, T)
+       + weights[1]*inlane_safety_cost(trajectory, vehicles, T)
+       + weights[2]*lanechange_safety_cost(trajectory, vehicles, T)
+       + weights[3]*lanechange_cost(trajectory)
+       + weights[4]*doublelanechange_safety_cost(trajectory, vehicles, T);
+}
+
+
+// Generates a jerk-minimizing trajectory (JMT)
+vector<vector<double>> generate_trajectory(double begin_s, double begin_d, double begin_speed, double tar_s, double tar_d, double tar_speed,
+                              vector<double> map_waypoints_s, vector<double> map_waypoints_x, vector<double> map_waypoints_y,
+                              tk::spline x_spl, tk::spline y_spl, double T) {
+  vector<double> start_s = {begin_s, begin_speed, 0.0};
+  vector<double> end_s = {tar_s, tar_speed, 0.0};
+  vector<double> start_d = {begin_d, 0.0, 0.0};
+  vector<double> end_d = {tar_d, 0.0, 0.0};
+  /*
+  std::cout << "Generating trajectory..." << std::endl;
+  std::cout << "Start s: " << begin_s << std::endl;
+  std::cout << "Target s: " << tar_s << std::endl;
+  std::cout << "Start d: " << begin_d << std::endl;
+  std::cout << "Target d: " << tar_d << std::endl;
+  std::cout << "Start speed: " << begin_speed << std::endl;
+  std::cout << "Target speed: " << tar_speed << std::endl;
+  //std::cout << "----------------------" << std::endl;
+  */
+  vector<double> JMTs_coeffs = JMT(start_s, end_s, T);
+  vector<double> JMTd_coeffs = JMT(start_d, end_d, T);
+  int NT = int(floor(T*50));
+  double prev_s;
+  vector<double> nx_vals, ny_vals, s_vals, d_vals;
+  for (int i=1; i<=NT; ++i){
+    double dt = float(i)*T/float(NT);
+    double JMTs = JMTs_coeffs[0] + JMTs_coeffs[1]*dt + JMTs_coeffs[2]*dt*dt + 
+                  JMTs_coeffs[3]*dt*dt*dt + JMTs_coeffs[4]*dt*dt*dt*dt + JMTs_coeffs[5]*dt*dt*dt*dt*dt;
+    double JMTd = JMTd_coeffs[0] + JMTd_coeffs[1]*dt + JMTd_coeffs[2]*dt*dt + 
+                   JMTd_coeffs[3]*dt*dt*dt + JMTd_coeffs[4]*dt*dt*dt*dt + JMTd_coeffs[5]*dt*dt*dt*dt*dt;
+    vector<double> xy = getXY(fmod(JMTs, max_s), JMTd, map_waypoints_s, map_waypoints_x, map_waypoints_y, x_spl, y_spl);
+    nx_vals.push_back(xy[0]);
+    ny_vals.push_back(xy[1]);
+    s_vals.push_back(JMTs);
+    d_vals.push_back(JMTd);
+  }
+  return {nx_vals, ny_vals, s_vals, d_vals};
+}
+
+
+vector<double> getXY(double s, double d, const vector<double> &maps_s, 
+                     const vector<double> &maps_x, 
+                     const vector<double> &maps_y,
+                     tk::spline x_spl, tk::spline y_spl) {
+  /**
+   * Transforms from Frenet s,d coordinates to Cartesian x,y
+   * Replaced provided linear interpolation with splines to be able to
+   * convert a JMT in Frenet to Cartesian without getting discontinuities
+   * from the convertion.
+   */
+  double seg_x = x_spl(s);
+  double seg_y = y_spl(s);
+  // finite differencing on the spline to get the heading
+  double car_heading = atan2(y_spl(s+0.01) - y_spl(s-0.01), x_spl(s+0.01) - x_spl(s-0.01));
+  double perp_heading = car_heading-pi()/2;
+  double x = seg_x + d*cos(perp_heading);
+  double y = seg_y + d*sin(perp_heading);
+  return {x,y};
+}
+
+
+vector<double> JMT(vector<double> &start, vector<double> &end, double T) {
+  /**
+   * Calculates the Jerk Minimizing Trajectory coefficients that connects the initial state
+   * to the final state in time T.
+   *
+   * @param start - the vehicles start location given as a length three array
+   *   corresponding to initial values of [s, s_dot, s_double_dot]
+   * @param end - the desired end state for vehicle. Like "start" this is a
+   *   length three array.
+   * @param T - The duration, in seconds, over which this maneuver should occur.
+   *
+   * @output an array of length 6, each value corresponding to a coefficent in 
+   *   the polynomial:
+   *   s(t) = a_0 + a_1 * t + a_2 * t**2 + a_3 * t**3 + a_4 * t**4 + a_5 * t**5
+   *
+   * EXAMPLE
+   *   > JMT([0, 10, 0], [10, 10, 0], 1)
+   *     [0.0, 10.0, 0.0, 0.0, 0.0, 0.0]
+   */
+   
+   double si = start[0];
+   double sidot = start[1];
+   double sidotdot = start[2];
+   double sf = end[0];
+   double sfdot = end[1];
+   double sfdotdot = end[2];
+   
+   double a0 = si;
+   double a1 = sidot;
+   double a2 = sidotdot/2.;
+   
+   Vector3f b;
+   b << sf - (si + sidot*T + sidotdot*T*T/2.),
+        sfdot - (sidot+sidotdot*T),
+        sfdotdot - sidotdot;
+   Matrix3f A;
+   A << T*T*T, T*T*T*T, T*T*T*T*T,
+        3*T*T, 4*T*T*T, 5*T*T*T*T,
+        6*T, 12*T*T, 20*T*T*T;
+        
+   Vector3f x = A.colPivHouseholderQr().solve(b);
+   double a3 = x[0];
+   double a4 = x[1];
+   double a5 = x[2];
+  return {a0,a1,a2,a3,a4,a5};
+}
+
 
 // Calculate distance between two points
 double distance(double x1, double y1, double x2, double y2) {
@@ -58,6 +317,7 @@ int ClosestWaypoint(double x, double y, const vector<double> &maps_x,
   return closestWaypoint;
 }
 
+
 // Returns next waypoint of the closest waypoint
 int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x, 
                  const vector<double> &maps_y) {
@@ -80,6 +340,7 @@ int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x,
 
   return closestWaypoint;
 }
+
 
 // Transform from Cartesian x,y coordinates to Frenet s,d coordinates
 vector<double> getFrenet(double x, double y, double theta, 
@@ -124,34 +385,6 @@ vector<double> getFrenet(double x, double y, double theta,
   frenet_s += distance(0,0,proj_x,proj_y);
 
   return {frenet_s,frenet_d};
-}
-
-// Transform from Frenet s,d coordinates to Cartesian x,y
-vector<double> getXY(double s, double d, const vector<double> &maps_s, 
-                     const vector<double> &maps_x, 
-                     const vector<double> &maps_y) {
-  int prev_wp = -1;
-
-  while (s > maps_s[prev_wp+1] && (prev_wp < (int)(maps_s.size()-1))) {
-    ++prev_wp;
-  }
-
-  int wp2 = (prev_wp+1)%maps_x.size();
-
-  double heading = atan2((maps_y[wp2]-maps_y[prev_wp]),
-                         (maps_x[wp2]-maps_x[prev_wp]));
-  // the x,y,s along the segment
-  double seg_s = (s-maps_s[prev_wp]);
-
-  double seg_x = maps_x[prev_wp]+seg_s*cos(heading);
-  double seg_y = maps_y[prev_wp]+seg_s*sin(heading);
-
-  double perp_heading = heading-pi()/2;
-
-  double x = seg_x + d*cos(perp_heading);
-  double y = seg_y + d*sin(perp_heading);
-
-  return {x,y};
 }
 
 #endif  // HELPERS_H
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
+#include <algorithm>
 
 // for convenience
 using nlohmann::json;
@@ -22,9 +24,11 @@ int main() {
   vector<double> map_waypoints_s;
   vector<double> map_waypoints_dx;
   vector<double> map_waypoints_dy;
+  vector<double> wps, wpx, wpy;
 
   // Waypoint map to read from
   string map_file_ = "../data/highway_map.csv";
+
   // The max s value before wrapping around the track back to 0
   double max_s = 6945.554;
 
@@ -48,10 +52,25 @@ int main() {
     map_waypoints_s.push_back(s);
     map_waypoints_dx.push_back(d_x);
     map_waypoints_dy.push_back(d_y);
+    wps.push_back(s);
+    wpx.push_back(x);
+    wpy.push_back(y);
   }
+  wps.push_back(max_s);
+  wpx.push_back(map_waypoints_x[0]);
+  wpy.push_back(map_waypoints_y[0]);
+
+  tk::spline x_spl, y_spl;
+  x_spl.set_points(wps, wpx);
+  y_spl.set_points(wps, wpy);
+ 
+  double prev_end_path_s = 0;
+  double prev_end_path_d = 0;
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+               &map_waypoints_dx,&map_waypoints_dy,
+               &prev_end_path_s, &prev_end_path_d,
+               &x_spl, &y_spl, max_s]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -80,25 +99,141 @@ int main() {
           // Previous path data given to the Planner
           auto previous_path_x = j[1]["previous_path_x"];
           auto previous_path_y = j[1]["previous_path_y"];
-          // Previous path's end s and d values 
+          // Previous path's end s and d values
+          // Comment: For some reason these don't give me the s and d coordinates, 
+          //          which I passed to the simulator last. Workaround with global
+          //          vars prev_end_path{s,d}.
           double end_path_s = j[1]["end_path_s"];
           double end_path_d = j[1]["end_path_d"];
 
           // Sensor Fusion Data, a list of all other cars on the same side 
           //   of the road.
           auto sensor_fusion = j[1]["sensor_fusion"];
+          vector<vehicle> vehicles;
+          for (int i=0; i<sensor_fusion.size(); i++){
+            vehicle v;
+            v.id = i;
+            double vx = sensor_fusion[i][3];
+            double vy = sensor_fusion[i][4];
+            v.v = sqrt(vx*vx + vy*vy);
+            v.s = sensor_fusion[i][5];
+            v.d = sensor_fusion[i][6];
+            vehicles.push_back(v);
+          }
 
           json msgJson;
 
           vector<double> next_x_vals;
           vector<double> next_y_vals;
+          
+          int path_size = previous_path_x.size();
+          for (int i=0; i<path_size; ++i){
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
 
-          /**
-           * TODO: define a path made up of (x,y) points that the car will visit
-           *   sequentially every .02 seconds
-           */
+          double last_x, last_y, nlast_x, nlast_y, last_angle, last_speed;
+          if (path_size < 2){
+            last_x = car_x;
+            last_y = car_y;
+            last_angle = deg2rad(car_yaw);
+            last_speed = car_speed;
+            prev_end_path_s = car_s;
+            prev_end_path_d = car_d;
+          } else {
+            last_x = previous_path_x[path_size-1];
+            last_y = previous_path_y[path_size-1];
+            nlast_x = previous_path_x[path_size-2];
+            nlast_y = previous_path_y[path_size-2];
+            last_angle = atan2(last_y-nlast_y,last_x-nlast_x);
+            last_speed = distance(last_x, last_y, nlast_x, nlast_y) / 0.02;
+          }
 
+          if (path_size < 30){
+            int next_waypoint_ind = NextWaypoint(last_x, last_y, rad2deg(last_angle), map_waypoints_x, map_waypoints_y);
+            std::cout << "Current car s: " << car_s << std::endl;
+            std::cout << "Current waypoint id: " << next_waypoint_ind << std::endl;
 
+            double begin_s = prev_end_path_s;
+            double begin_d = prev_end_path_d;
+            double begin_speed = last_speed;
+
+            int current_lane = round((begin_d - 2.0)/4.0);
+            // Possible lanes are {0,1,2} = {left, center, right}
+            int tar_lane;
+            double tar_speed, tar_d;
+            
+            // Start: Behavior planner
+            vector<string> states = {"KL", "LCL", "LCR", "DLCL", "DLCR"};
+            vector<double> speed_states = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19}; // [m/s]
+            vector<string> possible_next_states = get_possible_next_states(current_lane);
+
+            string best_state;
+            double best_speed;
+            vector<vector<double>> best_trajectory;
+            double cost = 9999;
+            for (int i=0; i<possible_next_states.size(); i++){
+              for (int s=0; s<speed_states.size(); s++){
+                string state = possible_next_states[i];           
+                if (state == "KL"){
+                  tar_lane = current_lane;
+                } else if (state == "LCL"){
+                  tar_lane = current_lane - 1;
+                } else if (state == "LCR"){
+                  tar_lane = current_lane + 1;
+                } else if (state == "DLCL"){
+                  tar_lane = current_lane - 2;
+                } else if (state == "DLCR"){
+                  tar_lane = current_lane + 2;
+                }
+                tar_speed = speed_states[s];
+                tar_d = 2.0 + 4.0*tar_lane-0.2;
+
+                // travel time of the generated trajectory in seconds
+                double T;
+                if (tar_lane == current_lane){
+                  // longer maneuver time for larger speed differences, at least 0.5 sec
+                  T = std::max(fabs(tar_speed - begin_speed)/5.5, 0.5);
+                } else if ((tar_lane == current_lane + 1) || (tar_lane == current_lane - 1)){
+                  // minimum of 2.5 sec for a single lane change maneuver
+                  T = std::max(fabs(tar_speed - begin_speed)/5.5, 2.5);
+                } else {
+                  // minimum of 4 sec for a double lane change maneuver
+                  T = std::max(fabs(tar_speed - begin_speed)/5.5, 4.0);
+                }
+                double tar_s = begin_s + T*(tar_speed + begin_speed)/2.0;
+                
+                // Generate trajectory
+                vector<vector<double>> trajectory = generate_trajectory(begin_s, begin_d, begin_speed, tar_s, tar_d, 
+                                                              tar_speed, map_waypoints_s, map_waypoints_x, map_waypoints_y,
+                                                              x_spl, y_spl, T);
+                
+                // Calculate cost
+                // Weights: {efficiency, inlane safety, lane change safety, lane switch penalty, double lane change safety}
+                vector<double> weights = {1, 2, 100, 0.01, 100};
+                double state_cost = eval_cost(trajectory, vehicles, weights, T);
+                std::cout << "Cost of state " << state << " with speed " << tar_speed << "m/s: " << state_cost << std::endl;
+                if (state_cost < cost){
+                  cost = state_cost;
+                  best_state = state;
+                  best_speed = tar_speed;
+                  prev_end_path_s = fmod(tar_s, max_s);
+                  prev_end_path_d = tar_d;
+                  best_trajectory = trajectory;
+                }
+              }
+            }
+            std::cout << "Planner output state: " << best_state << std::endl;
+            std::cout << "Planner output speed: " << best_speed << "m/s" << std::endl;
+            std::cout << "------------------------------" << std::endl;
+            // End: Behavior planner
+
+            // Append best trajectory to the path
+            for (int i=0; i<best_trajectory[0].size(); ++i){
+              next_x_vals.push_back(best_trajectory[0][i]);
+              next_y_vals.push_back(best_trajectory[1][i]);
+            }
+          }
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
